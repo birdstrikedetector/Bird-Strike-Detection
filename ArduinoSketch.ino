98d64c53f23ab472b1845641385c7cd03ab2acc7
birdstrikedetector <birdstrikedetector@gmail.com>
2025-11-10 22:12:58 -0500

    Create ArduinoSketch.ino

---
+++ ArduinoSketch.ino
@@ -0,0 +1,96 @@
+#include <WiFiNINA.h>
+#include <ArduinoHttpClient.h>
+
+WiFiClient wifiClient;
+
+const char* ssid = "AirPennNet-Device";
+const char* password = "penn1740wifi";
+
+
+const char testPing[] = "test.mosquitto.org"; // Arduino Nano pings this address to ensure Wifi is connected
+
+HttpClient client(wifiClient, "10.103.222.105", 80);  // your Pi IP, port
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial); // Wait for Serial monitor
+  Serial.println("Starting...");
+
+  connectWifi();
+  long pingResult = WiFi.ping(testPing);
+  if (pingResult >= 0) {
+    Serial.print("Ping successful! RTT = ");
+    Serial.print(pingResult);
+    Serial.println(" ms");
+  } else {
+    Serial.println("Ping failed!");
+  }
+
+  saveVideo();
+}
+
+void loop() {
+  //sendMessage("Hello Raspberry PI!!");
+
+  //getHealth();
+  //saveVideo();
+  //delay(10000);
+}
+
+// Connect to WiFi
+void connectWifi() {
+  Serial.print("Connecting to WiFi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  int retries = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+    retries++;
+    if (retries > 20) {
+      Serial.println("Failed to connect to WiFi.");
+      return;
+    }
+  }
+  Serial.println("\nWiFi connected!");
+}
+
+void sendMessage(String message) {
+  client.beginRequest();
+  client.post("/message");
+  client.sendHeader("Content-Type", "text/plain");
+  client.sendHeader("Content-Length", message.length());
+  client.beginBody();
+  client.print(message);
+  client.endRequest();  
+}
+
+
+void saveVideo() {
+  client.post("/save");
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+
+  Serial.print("Response: ");
+  Serial.println(response);
+}
+
+void getHealth() {
+  client.get("/health");
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+
+  Serial.print("Response: ");
+  Serial.println(response);
+
+  client.stop();
+}
