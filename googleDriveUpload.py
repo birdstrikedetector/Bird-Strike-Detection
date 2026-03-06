b91acb79d0efaa9b6de619654b8120e26ee5139e
birdstrikedetector <birdstrikedetector@gmail.com>
2026-03-06 07:58:19 -0500

    Update googleDriveUpload.py
    fixed filepath error

--- googleDriveUpload.py
+++ googleDriveUpload.py
@@ -23,7 +23,7 @@ def get_drive_service():
                 '../../credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('../token.json', 'w') as token:
+        with open('../../token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('drive', 'v3', credentials=creds)
